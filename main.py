cb7a832d3d8d8dcc6aecd0bdcb44505ab01e385e
yandex <dvorfchihunperdun@gmail.com>
2024-11-18 19:10:06 +0300

    добавили функцию

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 def print_hi(name):
     print(f'Hi, {name}')
     print('Изменения в файле')
+    #  0e41bae80473b133b2591f6d12c563f7295781c2
     print('еще что то меняю')
+    #  228d3474a5eed97b11fb69325540652589b87acb
 
+
+def calc(a, b):
+    return a + b
 
 
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(calc(10, 15))
